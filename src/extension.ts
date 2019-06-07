5cc790c2d7b4f58098e5f58da72e4179be16b802
wilson <wilsonggodoi@gmail.com>
2019-06-07 15:42:18 -0300

    Init on es6 features

--- src/extension.ts
+++ src/extension.ts
@@ -57,7 +57,54 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposableES6 = vscode.commands.registerCommand('extension.generateGettersAndSettersES6', () => {
+        // The code you place here will be executed every time your command is executed
+
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        let code = ``;
+        let reverse: boolean = false;
+
+        for (let selection of editor.selections) {
+            reverse = selection.isReversed;
+        }
+
+        let selections: vscode.Selection[];
+        if (reverse) {
+            selections = editor.selections.reverse();
+        } else {
+            selections = editor.selections;
+        }
+
+        for (let selection of selections) {
+            code += editor.document.getText(selection);
+            code += `\n`;
+        }
+
+        let text = code;
+
+        if (text.length < 1) {
+            vscode.window.showErrorMessage('No selected properties.');
+            return;
+        }
+
+        try {
+            var getterAndSetter = createGetterAndSetterES6(text);
+            editor.edit(e => e.insert(selections[selections.length - 1].end, getterAndSetter));
+        }
+        catch (error) {
+            console.log(error);
+            vscode.window.showErrorMessage('Something went wrong! Try that the properties are in this format: "private String name;"');
+        }
+    });
+
     context.subscriptions.push(disposable);
+    context.subscriptions.push(disposableES6);
 }
 
 function toCamelCase(str: string) {
@@ -65,6 +112,84 @@ function toCamelCase(str: string) {
 }
 
 function createGetterAndSetter(textProperties: string) {
+    let rows = textProperties.split('\n').map(x => x.replace(';', ''));
+    let properties: Array<string> = [];
+
+    for (let row of rows) {
+        if (row.trim() !== "") {
+            properties.push(row);
+        }
+    }
+
+    let generatedCode = `
+`;
+    for (let p in properties) {
+        while (properties[p].startsWith(" ")) { properties[p] = properties[p].substr(1); }
+        while (properties[p].startsWith("\t")) { properties[p] = properties[p].substr(1); }
+        let words: Array<string> = [];
+
+        let rows = properties[p].split(" ").map(x => x.replace('\r\n', ''));
+        for (let row of rows) {
+            if (row.trim() !== '') {
+                words.push(row);
+            }
+        }
+        let type, attribute, Attribute = "";
+        let create = false;
+
+        // if words === ["private", "name:", "string"];
+        if (words.length === 3) {
+            let attributeArray = words[1].split(":");
+            type = words[2];
+            attribute = attributeArray[0];
+            Attribute = toCamelCase(attribute);
+
+            create = true;
+            // if words === ["private", "name:string"];
+        } else if (words.length === 2) {
+            let array = words[1].split(":");
+            type = array[1];
+            attribute = array[0];
+            Attribute = toCamelCase(attribute);
+            create = true;
+            // if words === ["private", "name", ":", "string"];
+        } else if (words.length === 4) {
+
+            let array: Array<string> = [];
+            for (let word of words) {
+                if (word !== ':') {
+                    array.push(word);
+                }
+            }
+            type = array[2].trim();
+            attribute = array[1];
+            Attribute = toCamelCase(attribute);
+            create = true;
+        } else {
+            vscode.window.showErrorMessage('Something went wrong! Try that the properties are in this format: "private name: string;"');
+            generatedCode = ``;
+            break;
+        }
+
+        if (create) {
+
+            let code = `
+    public ${(type === "Boolean" || type === "boolean" ? "is" : "get")}${Attribute}(): ${type} {
+        return this.${attribute};
+    }
+
+    public set${Attribute}(${attribute}: ${type}): void {
+        this.${attribute} = ${attribute};
+    }
+`;
+            generatedCode += code;
+        }
+    }
+
+    return generatedCode;
+}
+
+function createGetterAndSetterES6(textProperties: string) {
     let rows = textProperties.split('\n').map(x => x.replace(';', ''));
     let properties: Array<string> = [];
 
